2e734ab0644bb094104daddc7b5a2a81546ffaeb
atang618 <allentang2018@u.northwestern.edu>
2016-02-03 14:47:44 -0600

    more comments getters and setters called get[X]() and set[X], respectively main() calls run_tests()

--- vector2d.cpp
+++ vector2d.cpp
@@ -3,37 +3,38 @@
 // constructors
 Vector2d::Vector2d() : x_{0}, y_{0} {}; //default constructor
 Vector2d::Vector2d(double x, double y) {
-    // implement here
+    x_=x;
+    y_=y;
 }
 
 // getters: return the value of member variables x and y
 double Vector2d::get_x() const { //const indicates that the function cannot change the object it is called from
-    // implement here
+    return x_;
 
 }
 double Vector2d::get_y() const {
-    // implement here
+    return y_;
 
 
 }
 
 // setters: set the value of member variables x and y
 void Vector2d::set_x(double x) {
-    // implement here
+    x_=x;
 }
 void Vector2d::set_y(double y) {
-    // implement here
+    y_=y;
 }
 
 // helper function
 Vector2d add(const Vector2d& a, const Vector2d& b) { //const indicates that parameters that are const cannot be changed
-    // implement here
+    return Vector2d(a.get_x() + b.get_x(),a.get_y() + b.get_y());
 
 }
 
 // helper function
 Vector2d subtract(const Vector2d& a, const Vector2d& b) {
-    // implement here
+    return Vector2d(a.get_x() - b.get_x(),a.get_y() - b.get_y());
 }
 
 // operator overloading of '=='
@@ -44,8 +45,8 @@ bool operator==(const Vector2d& a, const Vector2d& b) {
 
 // operator overloading of '+' and '-'
 Vector2d operator+(const Vector2d& a, const Vector2d& b) {
-    // implement here
+    return Vector2d(a.get_x() + b.get_x(),a.get_y() + b.get_y());
 }
 Vector2d operator-(const Vector2d& a, const Vector2d& b) {
-    // implement here
+    return Vector2d(a.get_x() - b.get_x(),a.get_y() - b.get_y());
 }
